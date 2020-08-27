fb752d11808d19a3ebb372e5e3a24d9d5134b13a
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2020-08-27 16:55:06 +0200

    now using the existing class in collections for the dictwrapper

--- dictwrapper/wrapper.py
+++ dictwrapper/wrapper.py
@@ -1,38 +1,14 @@
+from collections import UserDict
 from collections.abc import MutableMapping
+from abc import ABC
 
 
-class DictWrapper(MutableMapping):
-    """A dictionary wrapper to allow customization"""
-
-    def __init__(self, *args, **kwargs):
-        self.store = dict()
-        self.update(dict(*args, **kwargs))
-
-    def __getitem__(self, key):
-        return self.store[key]
-
-    def __setitem__(self, key, value):
-        self.store[key] = value
-
-    def __delitem__(self, key):
-        del self.store[key]
-
-    def __iter__(self):
-        return iter(self.store)
-
-    def __len__(self):
-        return len(self.store)
-
-    def __contains__(self, item):
-        return item in self.store
-
-    def keys(self):
-        """Access the dictionary keys"""
-        return self.store.keys()
-
-    def values(self):
-        """Access the dictionary values"""
-        return self.store.values()
-
+class DictWrapper(UserDict):
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.store.__repr__()})"
+        return f"{self.__class__.__name__}({self.data.__repr__()})"
+
+
+class DictWrapperStub(MutableMapping, ABC):
+    def __init__(self, *args, **kwargs):
+        self.data = dict()
+        self.update(*args, **kwargs)
